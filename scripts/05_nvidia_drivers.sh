3280bb8a402a6d0eb4025279b2f9cf9205fd4234
Bjorn Mathisen <bcm27@live.com>
2025-06-08 23:29:46 -0500

    Update 05_nvidia_drivers.sh

--- scripts/05_nvidia_drivers.sh
+++ scripts/05_nvidia_drivers.sh
@@ -1,16 +1,12 @@
 #!/bin/bash
-
 # Do NOT download drivers from Nvidia’s website.
-# First, try installing via DNF.
-sudo dnf install -y akmod-nvidia
-sudo dnf install -y xorg-x11-drv-nvidia-cuda
+sudo dnf install kmodtool akmods mokutil openssl 
+sudo kmodgenca -a 
+sudo mokutil --import /etc/pki/akmods/certs/public_key.der 
+sudo reboot
+# MOK manager will ask you, if you want to proceed with boot, or import the key. Pick import the key, type in a password
+sudo dnf install gcc kernel-headers kernel-devel akmod-nvidia xorg-x11-drv-nvidia xorg-x11-drv-nvidia-libs xorg-x11-drv-nvidia-libs.i686 
+modinfo -F version nvidia 
 
-# Wait at least 5–10 minutes before rebooting to allow the kernel module to finish building.
-echo "Please wait 5–10 minutes before rebooting to allow the kernel module to finish building."
-
-# Check if the driver installed successfully.
-modinfo -F version nvidia
-
-# If you see a version number (e.g., 570.153.02), the installation was successful.
-# If not, see: 
 # [Reddit: Fedora Nvidia Secure Boot Guide](https://www.reddit.com/r/Fedora/comments/18bj1kt/fedora_nvidia_secure_boot/?utm_source=share&utm_medium=web3x&utm_name=web3xcss&utm_term=1&utm_content=share_button)
+
